26940246dac83f9c0ee9a080265f73111d534264
vinoaj <vinoaj@gmail.com>
2015-10-22 17:18:20 +0700

    sheets transformation example
    sheets transformation example

---
+++ sheets/transform_values.js
@@ -0,0 +1,37 @@
+/**
+ * @fileoverview Transform data in a Range
+ * @author vinoaj@gmail.com (Vinoaj Vijeyakumaar)
+ */
+
+/**@const */ var RE_CLEANUP = /undefined|NOT_FOUND/g
+/**@const */ var SOURCE_SHEET_NAME = 'Sheet1';
+
+
+var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+var sheet = spreadsheet.getSheetByName(SOURCE_SHEET_NAME);
+var dataRange = spreadsheet.getDataRange();
+var dataValues = dataRange.getValues();
+
+
+//Replace instances of "undefined" and "NOT_FOUND" with an empty string
+var cleanedValues = substituteValues(dataValues, RE_CLEANUP, "");
+dataRange.setValues(cleanedValues);
+
+
+ /**
+  * Replace values in an 2D array
+  * @param {array} 2D array to be transformed
+  * @param {re} regular expression to evaluate in the existing data
+  * @param {string} replacement string
+  */
+ function substituteValues(arr, re, replacement) {
+    for (var row in arr) {
+     var replacedVals = arr[row].map(function(origVal) {
+       return origVal.replace(re, replacement);
+     });
+
+     arr[row] = replacedVals;
+   }
+
+   return arr;
+ }
