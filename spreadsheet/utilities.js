c718c4cb7dba0b92207b9d735611beb9de81019f
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-02-05 20:12:04 +1100

    added clearFilterOnColumn

--- spreadsheet/utilities.js
+++ spreadsheet/utilities.js
@@ -20,8 +20,45 @@ function appendRow(sheet, dataArray) {
  * Unhides all hidden rows in a sheet.
  * @param {Sheet} sheet Sheet that row will be appended to
  */
-function showAllRows(sheet) {
+function unhideAllRows(sheet) {
     dataRangeValues = sheet.getDataRange().getValues();
     var nRows = dataRangeValues.length;
     sheet.showRows(1, nRows);
+}
+
+/**
+ * Remove filter on column X in a given sheet
+ * Adapted from http://vnjv.co/2nJgBKt (Author unknown)
+ * @param {Spreadsheet} spreadsheet 
+ * @param {Sheet} sheet 
+ * @param {number} colIx 
+ */
+function clearFilterOnColumn(spreadsheet, sheet, colIx) {
+    var filterSettings = {};
+
+    // Look at the entire sheet's data
+    // To restrict data use additional parameters as documented at 
+    //    https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets#GridRange
+    filterSettings.range = {
+        sheetId: sheet.getSheetId()
+    };
+
+    // Helpful documentation: 
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/request#SetBasicFilterRequest
+    // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets#BasicFilter
+    // https://developers.google.com/sheets/api/reference/rest/v4/FilterCriteria  
+    filterSettings.criteria = {};
+    filterSettings['criteria'][colIx] = {
+        'hiddenValues': ['']  // Don't hide anything
+    };
+
+    var request = {
+        "setBasicFilter": {
+            "filter": filterSettings
+        }
+    };
+
+    Sheets.Spreadsheets.batchUpdate(
+        { 'requests': [request] }, spreadsheet.getId()
+    );
 }
