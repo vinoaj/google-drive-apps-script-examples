4ed808fe279a837c684b7189e3745063cf14ba88
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-02-05 19:28:09 +1100

    added showAllRows()

--- spreadsheet/utilities.js
+++ spreadsheet/utilities.js
@@ -7,11 +7,21 @@
 
  /**
  * Append a new row of data to the spreadsheet
- * @param {Sheet} sheetRef Sheet that row will be appended to
+ * @param {Sheet} sheet Sheet that row will be appended to
  * @param {string[]} dataArray 1-dimensional array of data
  */
-function appendRow(sheetRef, dataArray) {
-    var lastRow = sheetRef.getLastRow();
-    var range = sheetRef.getRange(lastRow + 1, 1, 1, dataArray.length);
+function appendRow(sheet, dataArray) {
+    var lastRow = sheet.getLastRow();
+    var range = sheet.getRange(lastRow + 1, 1, 1, dataArray.length);
     range.setValues([dataArray]);
+}
+
+ /**
+ * Unhides all hidden rows in a sheet.
+ * @param {Sheet} sheet Sheet that row will be appended to
+ */
+function showAllRows(sheet) {
+    dataRangeValues = sheet.getDataRange().getValues();
+    var nRows = dataRangeValues.length;
+    sheet.showRows(1, nRows);
 }
