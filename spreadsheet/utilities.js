ac89f6dfd51437ad2545b07c414436c3faeba300
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-03-12 19:29:01 +1100

    setCellValue

--- spreadsheet/utilities.js
+++ spreadsheet/utilities.js
@@ -5,21 +5,46 @@
  */
 
 
- /**
- * Append a new row of data to the spreadsheet
- * @param {Sheet} sheet Sheet that row will be appended to
- * @param {string[]} dataArray 1-dimensional array of data
+/**
+ * Get the header row for a sheet
+ * @param {Sheet} sheet Sheet that the header row will be read from
+ * @param {number} rowIx Row index of the header. Defaults to 1 (first row).
  */
+function getHeaderRow(sheet, rowIx) {
+    var rowIx = rowIx || 1;
+    var headerRow = sheet.getRange(rowIx, 1, 1, sheet.getLastColumn());
+    return headerRow.getValues()[0];
+}
+
+
+/**
+* Append a new row of data to the spreadsheet
+* @param {Sheet} sheet Sheet that row will be appended to
+* @param {string[]} dataArray 1-dimensional array of data
+*/
 function appendRow(sheet, dataArray) {
     var lastRow = sheet.getLastRow();
     var range = sheet.getRange(lastRow + 1, 1, 1, dataArray.length);
     range.setValues([dataArray]);
 }
 
- /**
- * Unhides all hidden rows in a sheet.
- * @param {Sheet} sheet Sheet that row will be appended to
+
+/**
+ * Sets the value for a given cell
+ * @param {object} value The value to be inserted into the cell
+ * @param {Sheet} sheet The sheet which contains the cell
+ * @param {number} rowIx Row position - 1-based numbering
+ * @param {number} colIx Column position - 1-based numbering
  */
+function setCellValue(value, sheet, rowIx, colIx) {
+    sheet.getRange(rowIx, colIx).setValue(value);
+}
+
+
+/**
+* Unhides all hidden rows in a sheet.
+* @param {Sheet} sheet Sheet that row will be appended to
+*/
 function unhideAllRows(sheet) {
     dataRangeValues = sheet.getDataRange().getValues();
     var nRows = dataRangeValues.length;
@@ -48,8 +73,10 @@ function clearFilterOnColumn(spreadsheet, sheet, colIx) {
     // https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets#BasicFilter
     // https://developers.google.com/sheets/api/reference/rest/v4/FilterCriteria  
     filterSettings.criteria = {};
-    filterSettings['criteria'][colIx] = {
-        'hiddenValues': ['']  // Don't hide anything
+
+    // Don't hide anything
+    filterSettings.criteria[colIx] = {
+        'hiddenValues': []
     };
 
     var request = {
